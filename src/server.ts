7a67221e0f7428907a75bd09272b609ec27241d2
Emma <EmmaRussellKilburn@gmail.com>
2024-11-27 11:51:23 +0000

    read port from config

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,22 @@
 import compression from "compression";
 import cors from "cors";
 import express from "express";
+import * as path from "node:path";
+import {ConfigReader} from "./configReader";
+import {GroutConfig} from "./types";
 
 const app = express();
 app.use(cors());
 app.use(compression({ level: 9 })); // Use best compression
 
-// TODO: read from config file
-const PORT = 5000;
+const rootDir = path.resolve(path.join(__dirname, ".."));
+const configReader = new ConfigReader(path.join(rootDir, "data"));
+const { port } = configReader.readConfigFile("grout.config.json") as GroutConfig;
 
 app.get("/", (_req, res) => {
     res.status(200).json({ message: "Hello grout" });
 });
 
-app.listen(PORT, () => {
-    console.log(`Grout is running on port ${PORT}`);
+app.listen(port, () => {
+    console.log(`Grout is running on port ${port}`);
 });
---
+++ src/types.ts
@@ -0,0 +1,3 @@
+export interface GroutConfig {
+    port: number
+}
