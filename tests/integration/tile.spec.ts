ecdcc72ef83f193a312dfdc00943a780d823131d
Emma <EmmaRussellKilburn@gmail.com>
2024-12-07 13:50:21 +0000

    integration test

---
+++ tests/integration/tile.spec.ts
@@ -0,0 +1,22 @@
+import { describe, expect, test } from "vitest";
+import { grout } from "./integrationTest";
+
+describe("tile endpoint", () => {
+    test("returns tile data", async () => {
+        const response = await grout.get("/tile/gadm41/admin0/3/1/4");
+        expect(response.status).toBe(200);
+        expect(response.header["content-type"]).toBe("application/octet-stream");
+        expect(response.header["content-encoding"]).toBe("gzip");
+        expect(response.body.length).toBeGreaterThan(0);
+    });
+
+    test("returns 404 when dataset does not exist", async () => {
+        const response = await grout.get("/tile/nonexistent/admin0/3/1/4");
+        expect(response.status).toBe(404);
+    });
+
+    test("returns 404 when tile does not exist",  async () => {
+        const response = await grout.get("/tile/gadm41/admin0/3/1/9999");
+        expect(response.status).toBe(404);
+    });
+});
