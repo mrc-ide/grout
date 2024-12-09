c3a94e933358573cd45b4ad11adfc96a5f796f74
Emma <EmmaRussellKilburn@gmail.com>
2024-12-09 09:41:24 +0000

    more lint

--- src/db/tileDatabase.ts
+++ src/db/tileDatabase.ts
@@ -8,7 +8,7 @@ export class TileDatabase {
         this.db = new sqlite3.Database(dbFileName, sqlite3.OPEN_READONLY);
     }
 
-    getTileData(z: number, x: number, y: number): any {
+    getTileData(z: number, x: number, y: number): object {
         return new Promise((resolve, reject) => {
             this.db.get(
                 `SELECT tile_data FROM tiles WHERE zoom_level = ${z} AND tile_column = ${x} AND tile_row = ${y}`,
