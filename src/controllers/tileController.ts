dd25f579403750c8b86650aabd369963e1805b77
Emma <EmmaRussellKilburn@gmail.com>
2024-12-09 17:22:47 +0000

    comments

--- src/controllers/tileController.ts
+++ src/controllers/tileController.ts
@@ -1,6 +1,8 @@
 import {NextFunction, Request, Response} from "express";
-import { AppLocals } from "../types/app";
+import {AppLocals} from "../types/app";
 import asyncControllerHandler from "../errors/asyncControllerHandler";
+import {GroutError} from "../errors/groutError";
+import {ErrorType} from "../errors/errorType";
 
 export class TileController {
     static getTile = async (req: Request, res: Response, next: NextFunction) => {
@@ -8,7 +10,9 @@ export class TileController {
             const {dataset, level, z, x, y} = req.params;
             const {tileDatasets} = req.app.locals as AppLocals;
 
-            // TODO: check coords params are throw 400 GroutError if not ints
+            // TODO: check coords params and throw 400 GroutError if not ints
+            // TODO: also throw error if requested dataset or level not found
+            // TODO: also return JSON response if route is not matched (always return JSON response in all cases!)
             let tileData = null;
             if (tileDatasets[dataset] && tileDatasets[dataset][level]) {
                 const db = tileDatasets[dataset][level];
--- src/errors/asyncControllerHandler.ts
+++ src/errors/asyncControllerHandler.ts
@@ -1,6 +1,6 @@
-// This method should be used to wrap any async controller methods to ensure error handling is applied
 import { NextFunction } from "express";
 
+// This method should be used to wrap any async controller methods to ensure error handling is applied
 export default async (next: Function, method: NextFunction) => {
     try {
         await method();
