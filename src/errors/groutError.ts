e278c21b5481a7b5975901636138827a2d518345
Emma <EmmaRussellKilburn@gmail.com>
2024-12-18 17:04:04 +0000

    make GroutError status a getter

--- src/errors/groutError.ts
+++ src/errors/groutError.ts
@@ -1,14 +1,16 @@
 import { ErrorType, ErrorTypeStatuses } from "./errorType";
 
 export class GroutError extends Error {
-    status: number;
     errorType: ErrorType;
 
     constructor(message: string, errorType: ErrorType) {
         super(message);
 
         this.name = "GroutError";
-        this.status = ErrorTypeStatuses[errorType];
         this.errorType = errorType;
+    }
+
+    get status() {
+        return ErrorTypeStatuses[this.errorType];
     }
 }
