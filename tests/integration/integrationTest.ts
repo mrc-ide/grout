59be5d6e1e06a4ef1681b95eee327edf76183405
Emma <EmmaRussellKilburn@gmail.com>
2025-01-07 14:47:55 +0000

    update README

--- tests/integration/integrationTest.ts
+++ tests/integration/integrationTest.ts
@@ -8,26 +8,6 @@ const getSchema = async (schemaName) => {
     return await import (`../../schema/${schemaName}.schema.json`);
 };
 
-const buildFullSchema = (responseSchema: object, dataSchema: object) => {
-    // TODO: use ajv Schema type
-    // Combine the generic response schema with the expected inner data schema to get single
-    // full schema we can use to validate a response.
-    // This wouldn't be needed if we could ust $dynamicRef, but ajv implementation is incomplete:
-    // https://github.com/ajv-validator/ajv/issues/1573
-
-    // Expect responseSchema to define "oneOf" for "data" and replace any generic object type
-    // with expected data schema
-    if (!responseSchema.properties?.data?.oneOf) {
-        throw("Expected response schema to have properties.data.oneOf path");
-    }
-
-    // filter out generic object type from data property, and replace with data schema
-    const newOneOf = [...responseSchema.properties.data.oneOf]
-        .filter((schema) => schema != {type: "object"});
-    newOneOf.push(dataSchema);
-    responseSchema.properties.data.oneOf = newOneOf;
-}
-
 // Get data from server, expect successful result, validate against schema, and return body.data
 export const getData = async (url: string, dataSchemaName: string) => {
     const response = await grout.get(url);
