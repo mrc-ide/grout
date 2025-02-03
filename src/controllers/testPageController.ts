7a677898b41b8d8a25a00a1cbbdca9743767c3de
Emma <EmmaRussellKilburn@gmail.com>
2025-02-03 17:48:08 +0000

    use path join

--- src/controllers/testPageController.ts
+++ src/controllers/testPageController.ts
@@ -1,9 +1,10 @@
 import { Request, Response } from "express";
 import { AppLocals } from "../types/app.js";
+import * as path from "node:path";
 
 export class TestPageController {
     static getTestPage = (req: Request, res: Response) => {
         const { rootDir } = req.app.locals as AppLocals;
-        res.sendFile(`${rootDir}/static/test.html`); // TODO: use path
+        res.sendFile(path.join(rootDir, "static", "test.html"));
     };
 }
